edbdf0cb061a739625f861efac47b14e9be8addc
TOURNEUX Elias <55650958+eliasto@users.noreply.github.com>
2022-01-18 13:13:00 +0100

    Legals page added

--- pages/index.js
+++ pages/index.js
@@ -268,6 +268,11 @@ const Home = ({ posts }) => {
       <footer className="bg-white">
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
           <div className="flex justify-center space-x-6 md:order-2">
+          <Link href={"/legals"}>
+                            <a key={"legals"} className="text-gray-400 hover:text-gray-500">
+                                <span className="text-md hover:text-black cursor-pointer">Mentions légales</span>
+                            </a>
+                        </Link>
             {footer.map((item) => (
               <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
                 <span className="sr-only">{item.name}</span>
