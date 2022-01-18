194b682e86c30b7f7c93d286bdaf3bf2894e08ff
TOURNEUX Elias <55650958+eliasto@users.noreply.github.com>
2022-01-18 13:47:59 +0100

    bug in logout solved

--- pages/panel/admin/settings.js
+++ pages/panel/admin/settings.js
@@ -457,7 +457,6 @@ export default function Settings({ user, role, me }) {
                                             type="button"
                                             className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
                                             onClick={() => {
-                                                updateUser();
                                                 setOpen(false);
                                             }}
                                         >
