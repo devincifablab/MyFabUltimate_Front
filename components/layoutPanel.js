41946f683d7c7b81917e0c55d94a7e504e324773
CodyIsTheSenate <mathieuman78@gmail.com>
2022-03-30 18:11:42 +0200

    Add project version in front

--- components/layoutPanel.js
+++ components/layoutPanel.js
@@ -7,6 +7,8 @@ import { SelectorIcon } from "@heroicons/react/solid";
 import { useRouter } from "next/router";
 import { removeCookies } from "cookies-next";
 import { logout } from "../lib/function";
+import axios from "axios";
+let version = "1.0.0"
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -27,6 +29,23 @@ export default function LayoutPanel({ children, user, role, authorizations, titl
     if (role.length == 0 && pn.split('/')[2] == "admin") {
       router.push('/404');
     }
+    setTimeout(async()=>{
+      await axios({
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        url: process.env.API + '/api/version',
+      }).then((response) => {
+        if (response.status == 200) {
+          version = response.data.version;
+        }
+      })
+        .catch((error) => {
+          console.log(error);
+      })
+    }, 0)
   }, []);
   if (role.length == 0 && pn.split('/')[2] == "admin") {
     return ('');
@@ -273,7 +292,7 @@ export default function LayoutPanel({ children, user, role, authorizations, titl
               </div>
             </nav>
           </div>
-          <span className="text-xs text-gray-400 text-center">version 1.0 - fablab@devinci.fr</span>
+          <span className="text-xs text-gray-400 text-center">version { version } - fablab@devinci.fr</span>
         </div>
       </div>
       <div className="flex flex-col w-0 flex-1 overflow-hidden">
