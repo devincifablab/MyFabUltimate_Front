5579fc624b6fa7998cbf1219445fc6b6dc87f6c9
CodyIsTheSenate <mathieuman78@gmail.com>
2022-05-04 18:25:50 +0200

    Change rules page location

--- pages/rules.js
+++ pages/rules.js
@@ -1,77 +1,135 @@
-import React, {  } from "react";
-import Layout from "../components/layout";
-import Link from 'next/link';
+import Link from "next/link";
+import { Fragment, useEffect } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import {
+  ChevronRightIcon,
+  DotsVerticalIcon,
+  TrashIcon
+} from "@heroicons/react/solid";
+import 'moment/locale/fr'
+
+import LayoutPanel from "../components/layoutPanel";
+import { fetchAPIAuth, parseCookies } from "../lib/api";
+import router from "next/router";
+import Moment from "react-moment";
+import { getColor, getState, setZero } from "../lib/function";
+import { getCookie } from "cookies-next";
+import axios from "axios";
+import { ChevronDownIcon } from "@heroicons/react/outline";
+import { toast } from "react-toastify";
+import Seo from "../components/seo";
 import RulesText from '../components/rules'
 
-const footer = [
-    {
-        name: 'Facebook',
-        href: 'https://facebook.com/devinci.fablab',
-        icon: (props) => (
-            <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-                <path
-                    fillRule="evenodd"
-                    d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                    clipRule="evenodd"
-                />
-            </svg>
-        ),
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+async function validateRules(){
+  const jwt = getCookie('jwt');
+  await axios({
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
     },
-    {
-        name: 'Instagram',
-        href: 'https://instagram.com/devinci.fablab',
-        icon: (props) => (
-            <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-                <path
-                    fillRule="evenodd"
-                    d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                    clipRule="evenodd"
-                />
-            </svg>
-        ),
+    url: process.env.API + '/api/user/validateRules/',
+    headers: {
+      'dvflCookie': jwt
+    },
+  }).then((response) => {
+    if(response.status===200){
+      toast.success("Vous avez validé le règlement.", {
+        position: "top-right",
+        autoClose: 1500,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }else{
+      toast.warn("Vous avez déjà validé le règlement.", {
+        position: "top-right",
+        autoClose: 1500,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
-]
+    setTimeout(async ()=>{await axios({
+          method: 'GET',
+          headers: {
+            "dvflCookie": jwt
+          },
+          url: process.env.API + '/api/user/authorization',
+        }).then((response) => {
+          if(response.data.myFabAgent == 1){
+            router.push('/panel/admin')
+          } else {
+            router.push('/panel');
+          }
+        });
+      }, 1500)
+  })
+    .catch((error) => {
+      console.log(error);
+      toast.error("Une erreur est survenue lors de la validation.", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    })
 
-const Legals = () => {
+}
 
-    return (
-        <Layout>
-            {/*<Seo seo={homepage.seo} />*/}
-            
-            <div className="flex h-screen bg-gray-50">
-                <div className="max-w-3xl m-auto text-center space-y-3 ">
-                    <h1 className="font-semibold leading-6 text-xl">Avant d'accéder à MyFab, vous devez accepter notre règlement.</h1>
-                    <p className="font-light text-md text-gray-500">L'utilisation de MyFab étant réglementé, l'acceptation de ces règles est obligatoire. Vous pouvez à tout moment nous contacter à fablab@devinci.fr si vous souhaitez faire valoir vos droits.</p>
-                    
-                    <div className="prose max-w-4xl overflow-y-auto max-h-96 mx-5 p-5 shadow-sm block w-full ring-indigo-500 border-indigo-500 sm:text-sm border rounded-md text-left text-justify">
-                        <RulesText/>
-                    </div>
-                </div>
-            </div>
+export default function Rules({userNeedToAccept}) {
+  return(
+      <div className="flex h-screen bg-gray-50">
+        <div className="max-w-3xl m-auto text-center space-y-3 ">
+          <h1 className="font-semibold leading-6 text-xl">Avant d'accéder à MyFab, vous devez accepter notre règlement.</h1>
+          <p className="font-light text-md text-gray-500">L'utilisation de MyFab étant réglementé, l'acceptation de ces règles est obligatoire. Vous pouvez à tout moment nous contacter à fablab@devinci.fr si vous souhaitez faire valoir vos droits.</p>
+          
+          <div className="prose max-w-4xl overflow-y-auto max-h-96 mx-5 p-5 shadow-sm block w-full ring-indigo-500 border-indigo-500 sm:text-sm border rounded-md text-left text-justify">
+            <RulesText/>
+          </div>
+          {userNeedToAccept ?
+          (<button
+            type="button"
+            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={()=>{validateRules()}}>
+            J'ai lu et j'accepte le règlement
+          </button>) :
+          (<a
+            href="/"
+            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Retour
+          </a>
+          
+          )}
+        </div>
+      </div>
+  )
+}
 
-            <footer className="bg-white">
-                <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
-                    <div className="flex justify-center space-x-6 md:order-2">
-                        <Link href={"/legals"}>
-                            <a key={"legals"} className="text-gray-400 hover:text-gray-500">
-                                <span className="text-md hover:text-black cursor-pointer">Mentions légales</span>
-                            </a>
-                        </Link>
-                        {footer.map((item) => (
-                            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
-                                <span className="sr-only">{item.name}</span>
-                                <item.icon className="h-6 w-6" aria-hidden="true" />
-                            </a>
-                        ))}
-                    </div>
-                    <div className="mt-8 md:mt-0 md:order-1">
-                        <p className="text-center text-base text-gray-400">&copy; {new Date().getFullYear()} Devinci FabLab. Tous droits réservés.</p>
-                    </div>
-                </div>
-            </footer>
-        </Layout>
-    );
-};
+export async function getServerSideProps({ req }) {
+  const cookies = parseCookies(req);
+  const user = await fetchAPIAuth("/user/me", cookies.jwt);
+  let userNeedToAccept;
+  if (user.error) {
+    userNeedToAccept = false;
+  }else if(user.acceptedRule == 1){
+    userNeedToAccept = false;
+  }else{
+    userNeedToAccept = true;
+  }
 
-
-export default Legals;
+  return {
+    props: { userNeedToAccept }, // will be passed to the page component as props
+  }
+}
