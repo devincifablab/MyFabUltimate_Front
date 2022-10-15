59b2af2224284a3b94f4c445b096434f75081d19
CodyIsTheSenate <mathieuman78@gmail.com>
2022-10-15 16:47:09 +0200

    Add filters to tickets history

--- pages/panel/admin/history.js
+++ pages/panel/admin/history.js
@@ -1,75 +1,141 @@
-import { useState } from 'react'
-import LayoutPanel from '../../../components/layoutPanel'
-import NavbarAdmin from '../../../components/navbarAdmin'
-import Seo from '../../../components/seo'
-import TablesAdmin from '../../../components/tablesAdmin'
-import { fetchAPIAuth, parseCookies } from '../../../lib/api'
+import { useState, useEffect } from "react";
+import LayoutPanel from "../../../components/layoutPanel";
+import NavbarAdmin from "../../../components/navbarAdmin";
+import Seo from "../../../components/seo";
+import TablesAdmin from "../../../components/tablesAdmin";
+import { fetchAPIAuth, parseCookies } from "../../../lib/api";
 import { isUserConnected } from "../../../lib/function";
+import axios from "axios";
+import { getCookie } from "cookies-next";
+import { toast } from "react-toastify";
 
-export default function OverviewAdmin({ tickets, user, role, authorizations }) {
-  const originalTicket = tickets;
-  const [ticketResult, setTicketResult] = useState(tickets);
+export default function OverviewAdmin({ user, role, authorizations }) {
+  const [inputValue, setInputValue] = useState("");
+  const [maxPage, setMaxPage] = useState(1);
+  const [actualPage, setActualPage] = useState(0);
+  let newActualPage = 0;
+  const [ticketResult, setTicketResult] = useState([]);
 
-  function update(search){
-    if(search.length>1 && search[0] == '#'){
-      setTicketResult(tickets.filter(r=>r.id == search.split('#')[1]));
-    } else if(search.length >2){
-      setTicketResult(tickets.filter(r=>r.userName.includes(search)));
-    }
-    if(search.length <2){
-      setTicketResult(originalTicket);
-    }
+  useEffect(function () {
+    update();
+  }, []);
+
+  function nextPrevPage(addPage) {
+    if (actualPage + addPage < 0 || actualPage + addPage >= maxPage) return;
+    setActualPage(actualPage + addPage);
+    newActualPage = actualPage + addPage;
+    update();
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // send state to server with e.g. `window.fetch`
+    update(true);
+  }
+
+  async function update(newReserch) {
+    if (newReserch) setActualPage(0);
+    const jwt = getCookie("jwt");
+    await axios({
+      method: "get",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        dvflCookie: jwt,
+      },
+      url: process.env.API + "/api/ticket",
+      params: { page: newActualPage, inputValue },
+    })
+      .then((response) => {
+        setMaxPage(response.data.maxPage);
+        setTicketResult(response.data.values);
+      })
+      .catch(() => {
+        toast.error("Une erreur est survenue lors du chargement des tickets.", {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      });
   }
 
   return (
     <LayoutPanel user={user} role={role} authorizations={authorizations} titleMenu="Gestion des demandes">
-                  <Seo title={"Historique"} />
+      <Seo title={"Historique"} />
 
       <NavbarAdmin role={role} />
 
       <section className="">
         <div className="container px-4 mx-auto">
           <div className="flex flex-wrap -mx-4">
-
             {/* Tickets à traiter */}
             <div className="w-full md:px-6 mt-5 mb-8 lg:mb-0">
               <div className="flex flex-col rounded shadow-sm bg-white overflow-hidden">
                 <div className="mb-3 grow">
                   <div className="space-x-2">
-                    <div className="relative grow">
+                    <form onSubmit={handleSubmit} className="relative grow">
                       <div className="absolute inset-y-0 left-0 w-10 my-px ml-px flex items-center justify-center pointer-events-none rounded-l text-gray-500">
-                        <svg className="hi-solid hi-search inline-block w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" /></svg>
+                        <svg className="hi-solid hi-search inline-block w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                          <path
+                            fillRule="evenodd"
+                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
                       </div>
-                      <input 
-                      onChange={(e)=>update(e.target.value)}
-                      className="block border placeholder-gray-400 pr-3 py-2 leading-6 w-full rounded border-gray-200 focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 pl-10" type="text" placeholder="Rechercher un numéro de ticket (en commencant par #) ou un pseudonyme" />
-                    </div>
+                      <div class="w-full inline-flex">
+                        <input
+                          onChange={(e) => {
+                            setInputValue(e.target.value);
+                          }}
+                          className="filterInput block border placeholder-gray-400 pr-3 py-2 leading-6 w-full rounded border-gray-200 focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 pl-10 mr-2"
+                          type="text"
+                          placeholder="Rechercher un ticket"
+                        />
+                        <button type="submit" class="w-2/12 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => update(true)}>
+                          Rechercher
+                        </button>
+                      </div>
+                    </form>
                   </div>
                 </div>
               </div>
               <TablesAdmin tickets={ticketResult} isDone={false} />
+              <div class="grid place-items-center mb-10">
+                <div class="inline-flex mt-3">
+                  <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l rounded-r mr-2" onClick={() => nextPrevPage(-1)}>
+                    &lt;
+                  </button>
+                  <p class="inline-flex py-2 px-4">
+                    Pages&nbsp;<p class="font-bold">{actualPage + 1}</p>&nbsp;sur&nbsp;<p class="font-bold">{maxPage != 0 ? maxPage : 1}</p>
+                  </p>
+                  <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l rounded-r ml-2 mr-6" onClick={() => nextPrevPage(1)}>
+                    &gt;
+                  </button>
+                </div>
+              </div>
             </div>
-
           </div>
         </div>
       </section>
     </LayoutPanel>
-
-  )
+  );
 }
 
 export async function getServerSideProps({ req }) {
-
   const cookies = parseCookies(req);
   const user = await fetchAPIAuth("/user/me", cookies.jwt);
   const resUserConnected = isUserConnected(user);
-  if(resUserConnected) return resUserConnected;
-  const tickets = await fetchAPIAuth("/ticket", cookies.jwt);
+  if (resUserConnected) return resUserConnected;
   const role = await fetchAPIAuth("/user/role", cookies.jwt);
   const authorizations = await fetchAPIAuth("/user/authorization/", cookies.jwt);
 
   // Pass the data to our page via props
   return {
-    props: { tickets, user, role, authorizations }, // will be passed to the page component as props
-  }
+    props: { user, role, authorizations }, // will be passed to the page component as props
+  };
 }
--- pages/panel/admin/index.js
+++ pages/panel/admin/index.js
@@ -8,49 +8,48 @@ import { fetchAPIAuth, parseCookies } from "../../../lib/api";
 import { isUserConnected } from "../../../lib/function";
 
 export default function Admin({ tickets, user, role, authorizations }) {
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(function () {
     if (user.error != undefined || role.length == 0) {
-      router.push('/404');
+      router.push("/404");
     }
   }, []);
   if (user.error == undefined && role.length != 0) {
-    const openTicket = tickets.filter(r => r.isOpen === 1);
+    console.log(tickets.values);
+    const openTicket = tickets.values.filter((r) => r.isOpen === 1);
+    console.log(openTicket);
     return (
       <LayoutPanel user={user} role={role} authorizations={authorizations} titleMenu="Gestion des demandes">
-        <Seo title={"Administration"}  />
+        <Seo title={"Administration"} />
         <NavbarAdmin role={role} />
         <div className="md:py-8 md:px-6">
           <div className="container px-8 md:px-16 py-8 mx-auto bg-gradient-to-r from-blue-400 to-indigo-500">
-            <h2 className="text-2xl font-bold text-white">Bonjour, {user.firstName} 👋{" "}</h2>
-            <h3 className="text-md font-medium text-white">Il y a {openTicket.length} impression{openTicket.length > 1 ? 's' : ''} à traiter. N'hésite pas à t'en occuper !</h3>
+            <h2 className="text-2xl font-bold text-white">Bonjour, {user.firstName} 👋 </h2>
+            <h3 className="text-md font-medium text-white">
+              Il y a {openTicket.length} impression{openTicket.length > 1 ? "s" : ""} à traiter. N'hésite pas à t'en occuper !
+            </h3>
           </div>
         </div>
-        <div>
-
-        </div>
+        <div></div>
         <OverviewAdmin tickets={openTicket} />
       </LayoutPanel>
     );
-  }
-  else {
-    return ('')
+  } else {
+    return "";
   }
 }
 
 export async function getServerSideProps({ req }) {
-
   const cookies = parseCookies(req);
   const user = await fetchAPIAuth("/user/me", cookies.jwt);
   const resUserConnected = isUserConnected(user);
-  if(resUserConnected) return resUserConnected;
+  if (resUserConnected) return resUserConnected;
   const tickets = await fetchAPIAuth("/ticket", cookies.jwt);
   const role = await fetchAPIAuth("/user/role", cookies.jwt);
   const authorizations = await fetchAPIAuth("/user/authorization/", cookies.jwt);
 
   return {
     props: { tickets, user, role, authorizations }, // will be passed to the page component as props
-  }
+  };
 }
-
